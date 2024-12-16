f96bf81c4c5e5178cc7125b68c3e6396aa78a23b
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2024-12-16 15:43:48 -0300

    up

--- resources/views/app/layouts/header.blade.php
+++ resources/views/app/layouts/header.blade.php
@@ -15,7 +15,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <title>Gestão de Manutenção Fapolpa</title>
+    <title>Gestão de Manutenção</title>
 </head>
 
 <body>
