b11766d062bf9e46f48ccc5c25e476a452d8d0aa
Sidinei Da Rosa <70112853+SidineiDaRosa@users.noreply.github.com>
2024-12-17 14:34:34 -0300

    up

--- resources/views/site/navigation_bar.blade.php
+++ resources/views/site/navigation_bar.blade.php
@@ -153,6 +153,8 @@
             display: flex;
             font-size: 25px;
             font-family: 'Poppins', sans-serif;
+            color: rgb(0, 0, 0, 0.65);
+
 
         }
 
@@ -176,153 +178,216 @@
             margin-right: 2px;
         }
     </style>
+    <!------------------------------->
+    <!-- incio do cabeçalho navbar--->
+    <!------------------------------->
     <nav class="navbar" style="background-color: rgb(245, 246, 248);">
         <div class="menu" id="menu" style="background-color: rgb(245, 246, 248);">
             <div class="menu-item">
                 <a href="#">Home</a>
             </div>
-            <!-- inicício do menu-->
-            <div class="menu-item">
-                <a href="#">Sobre nós</a>
-                <!--Div que expande para baixo drop down-->
-                <div class="dropdown" style="background-color: rgb(245, 246, 248);">
-                    {{--------------Início continer box----------------------------------------}}
-                    <div class="container-box" style="background-color: rgb(245, 246, 248);">
-                        {{--Box 1--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Empresa</div>
-                                <div class="conteudo">
-                                    <span style="color:rgba(92, 92, 92, 0.8);">
-                                        Estamos empanhado em fornecer soluções <br>
-                                        Eficases, com tecnologia de ponta. <br>
-                                        Conjugando tecnologia e relacionado custo benefício, <br>
-                                        no segmento de automação de processos industriais.
-                                    </span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        {{--Box 2--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Pessoas</div>
-                                <div class="conteudo" >
-                                    <a href="" style="color:rgba(92, 92, 92, 0.8);">Nossa equipe</a>
-                                </div>
-                            </div>
-                        </div>
-                        {{--Box 3--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Sobre</div>
-                                <div class="conteudo">
-                                    <a href="#"></a>
-                                </div>
-                            </div>
+            <style>
+                /* Estilização do botão no canto superior direito */
+                .login-container {
+                    position: absolute;
+                    top: 10px;
+                    right: 20px;
+                }
+
+                .login-button {
+                    background-color: #4CAF50;
+                    color: white;
+                    padding: 10px 20px;
+                    text-decoration: none;
+                    border-radius: 5px;
+                    font-size: 14px;
+                }
+
+                .login-button:hover {
+                    background-color: #45a049;
+                }
+
+                .logout-button {
+                    background-color: #e74c3c;
+                    color: white;
+                    padding: 10px 20px;
+                    text-decoration: none;
+                    border-radius: 5px;
+                    font-size: 14px;
+                }
+
+                .logout-button:hover {
+                    background-color: #c0392b;
+                }
+            </style>
+</head>
+<!---------------------------------------------------
+     Incio do button login
+     ----------------------------------------------->
+
+<body>
+    <!-- Verificação de Login -->
+    <div class="login-container">
+        @auth
+        <span style="font-family: Arial, Helvetica, sans-serif;font-weight:300px;font-size:12px;">Bem-vindo, {{ Auth::user()->name }}!</span>
+        <a href="{{ route('logout') }}" class="logout-button"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+            Logout
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+        </form>
+        @else
+        <a href="{{ route('login_start') }}" class="login-button">Login</a>
+        @endauth
+    </div>
+    <!-- inicício do menu-->
+    <div class="menu-item">
+        <a href="#">Sobre nós</a>
+        <!--Div que expande para baixo drop down-->
+        <div class="dropdown" style="background-color: rgb(245, 246, 248);">
+            {{--------------Início continer box----------------------------------------}}
+            <div class="container-box" style="background-color: rgb(245, 246, 248);">
+                {{--Box 1--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Empresa</div>
+                        <div class="conteudo">
+                            <span style="color:rgba(92, 92, 92, 0.8);">
+                                Estamos empanhado em fornecer soluções <br>
+                                Eficases, com tecnologia de ponta. <br>
+                                Conjugando tecnologia e relacionado custo benefício, <br>
+                                no segmento de automação de processos industriais.
+                            </span>
+                            </a>
                         </div>
                     </div>
-                    {{--------------fim continer box----------------------------------------}}
                 </div>
-            </div>
-            <div class="menu-item">
-                <a href="#">Produtos e serviços</a>
-                <!--Div que expande para baixo-->
-                <div class="dropdown" style="background-color: rgb(245, 246, 248);">
-                    {{--------------Início continer box----------------------------------------}}
-                    <div class="container-box" style="background-color: rgb(245, 246, 248);">
-                        {{--Box 1--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Produtos</div>
-                                <p></p>
-                                <div class="conteudo">
-                                    <a href="">Produtos</a>
-                                </div>
-                                <div class="titulo" style="color:rgba(0, 0, 0, 0.7);">Suporte</div>
-                                <span style="color:rgba(92, 92, 92, 0.8);">
-                                    Eng. Sidinei da Rosa <br>
-                                    (46)99984-26-64 <br>
-                                    Email:Sidineidarosa201@gmail.com
-                                </span>
-
-                            </div>
-                        </div>
-                        {{--Box 2--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Sistemas de Gestão</div>
-                                <p></p>
-                                <div class="conteudo">
-                                    <a href="{{ route('login_start') }}" class="title-menu" caption="erp" style="color:#5C5C5C;">CMMS ManWEB SaaS</a>
-                                </div>
-                            </div>
-                        </div>
-                        {{--Box 3--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Serviços</div>
-                                <span style="color:rgba(92, 92, 92, 0.8);">
-                                    <li>Projeto e montagem de paineis elétricos BT, AT</li>
-                                    <li>Projetos retrofiting de máquinas</li>
-                                    <li>Programação de CLPs, IHMs, Supervisáorios SCADA <br>
-                                        com aplicação de DB, IoT</li>
-                                    <li>Projetos elétricos</li>
-                                    <li>Prestação de serviços de egenharia</li>
-                                    <li>Laudos elétricos</li>
-                                    <li>Instalações elétricas industriais</li>
-                                </span>
-
-
-                            </div>
+                {{--Box 2--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Pessoas</div>
+                        <div class="conteudo">
+                            <a href="" style="color:rgba(92, 92, 92, 0.8);">Nossa equipe</a>
                         </div>
                     </div>
-                    {{--------------fim continer box----------------------------------------}}
                 </div>
-            </div>
-            <div class="menu-item">
-                <a href="#">Contato</a>
-                <!--Div que expande para baixo-->
-                <div class="dropdown" style="background-color: rgb(245, 246, 248);">
-                    {{--------------Início continer box----------------------------------------}}
-                    <div class="container-box" style="background-color: rgb(245, 246, 248);">
-                        {{--Box 1--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Localização</div>
-                                Avenida Governador Viriato Parigot de souza <br>
-                                N° 2184 <br>
-                                Palmas <br>
-                                Paraná <br>
-                                CEP 85692392.
-                            </div>
-                        </div>
-                        {{--Box 2--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">#</div>
-                                <p></p>
-                                <div class="conteudo">
-                                    #
-                                </div>
-                            </div>
-                        </div>
-                        {{--Box 3--}}
-                        <div class="item" style="background-color: rgb(245, 246, 248);">
-                            <div class="box-conteudo">
-                                <div class="titulo">Notícias</div>
-                                <p></p>
-
-                                <div class="conteudo">
-                                    Revistas e lançamentos de produtos
-                                </div>
-                            </div>
+                {{--Box 3--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Sobre</div>
+                        <div class="conteudo">
+                            <a href="#"></a>
                         </div>
                     </div>
-                    {{--------------fim continer box----------------------------------------}}
                 </div>
             </div>
+            {{--------------fim continer box----------------------------------------}}
         </div>
+    </div>
+    <div class="menu-item">
+        <a href="#">Produtos e serviços</a>
+        <!--Div que expande para baixo-->
+        <div class="dropdown" style="background-color: rgb(245, 246, 248);">
+            {{--------------Início continer box----------------------------------------}}
+            <div class="container-box" style="background-color: rgb(245, 246, 248);">
+                {{--Box 1--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Produtos</div>
+                        <p></p>
+                        <div class="conteudo">
+                            <a href="">Produtos</a>
+                        </div>
+                        <div class="titulo">Suporte</div>
+                        <span style="color:rgba(92, 92, 92, 0.8);">
+                            Eng. Sidinei da Rosa <br>
+                            (46)99984-26-64 <br>
+                            Email:Sidineidarosa201@gmail.com
+                        </span>
+
+                    </div>
+                </div>
+                {{--Box 2--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Sistemas de Gestão</div>
+                        <p></p>
+                        <div class="conteudo">
+                            <a href="{{ route('login_start') }}" class="title-menu" caption="erp" style="color:#5C5C5C;">CMMS ManWEB SaaS</a>
+                        </div>
+                    </div>
+                </div>
+                {{--Box 3--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Serviços</div>
+                        <span style="color:rgba(92, 92, 92, 0.8);">
+                            <li>Projeto e montagem de paineis elétricos BT, AT</li>
+                            <li>Projetos retrofiting de máquinas</li>
+                            <li>Programação de CLPs, IHMs, Supervisáorios SCADA <br>
+                                com aplicação de DB, IoT</li>
+                            <li>Projetos elétricos</li>
+                            <li>Prestação de serviços de egenharia</li>
+                            <li>Laudos elétricos</li>
+                            <li>Instalações elétricas industriais</li>
+                        </span>
+
+
+                    </div>
+                </div>
+            </div>
+            {{--------------fim continer box----------------------------------------}}
+        </div>
+    </div>
+    <div class="menu-item">
+        <a href="#">Contato</a>
+        <!--Div que expande para baixo-->
+        <div class="dropdown" style="background-color: rgb(245, 246, 248);">
+            {{--------------Início continer box----------------------------------------}}
+            <div class="container-box" style="background-color: rgb(245, 246, 248);">
+                {{--Box 1--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Localização</div>
+                        <span style="color:rgba(92, 92, 92, 0.8);font-family:Roboto;
+                        font:-weigth 300px; "
+                        >
+                            Avenida Governador Viriato Parigot de souza <br>
+                            N° 2184 <br>
+                            Palmas <br>
+                            Paraná <br>
+                            CEP 85692392.
+                        </span>
+
+                    </div>
+                </div>
+                {{--Box 2--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">#</div>
+                        <p></p>
+                        <div class="conteudo">
+                            #
+                        </div>
+                    </div>
+                </div>
+                {{--Box 3--}}
+                <div class="item" style="background-color: rgb(245, 246, 248);">
+                    <div class="box-conteudo">
+                        <div class="titulo">Notícias</div>
+                        <p></p>
+
+                        <div class="conteudo">
+                            Revistas e lançamentos de produtos
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{--------------fim continer box----------------------------------------}}
+        </div>
+    </div>
+    </div>
     </nav>
     <script>
         function myFunction() {
@@ -336,4 +401,4 @@
     </script>
     {{-----------------------------------------fim nav bar-------------------------}}
 
-</head>
+    </head>
